624965bc168c7585c5357afea9f1c4fe2516a6dc
sofiia <sofiia.teplova@gmail.com>
2019-06-18 16:50:43 +0300

    styles update

---
+++ public/input.js
@@ -0,0 +1,32 @@
+// jQuery('<div class="q-nav"><div class="q-button q-up">+</div><div class="q-button q-down">-</div></div>').insertAfter('.wa');
+// jQuery('.quantity').each(function() {
+//   var spinner = jQuery(this),
+//   var input = spinner.find('.wa'),
+//   var btnUp = spinner.find('.q-up'),
+//   var btnDown = spinner.find('.q-down'),
+//   var min = input.attr('min'),
+//   var max = input.attr('max');
+//
+//   btnUp.click(function() {
+//     var oldValue = parseFloat(input.val());
+//     if (oldValue >= max) {
+//       var newVal = oldValue;
+//     } else {
+//       var newVal = oldValue + 1;
+//     }
+//     spinner.find(".wa").val(newVal);
+//     spinner.find(".wa").trigger("change");
+//   });
+//
+//   btnDown.click(function() {
+//     var oldValue = parseFloat(input.val());
+//     if (oldValue <= min) {
+//       var newVal = oldValue;
+//     } else {
+//       var newVal = oldValue - 1;
+//     }
+//     spinner.find(".wa").val(newVal);
+//     spinner.find(".wa").trigger("change");
+//   });
+//
+// });
