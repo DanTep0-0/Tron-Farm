e2285aee84f3aa6ae075e7c7251f8dfeb8b10672
DanTep <danya.teplov2004@gmail.com>
2019-06-18 23:00:27 +0300

    update

--- src/components/TronLinkGuide/index.js
+++ src/components/TronLinkGuide/index.js
@@ -5,12 +5,6 @@ import './TronLinkGuide.scss';
 
 const WEBSTORE_URL = 'https://chrome.google.com/webstore/detail/ibnejdfjmmkpcnlpebklmnkoeoihofec/';
 
-const logo = (
-    <div className='logo'>
-        <img src={ TronLinkLogo } alt='TronLink logo' />
-    </div>
-);
-
 const openTronLink = () => {
     window.open(WEBSTORE_URL, '_blank');
 };
@@ -23,29 +17,35 @@ const TronLinkGuide = props => {
     if(!installed) {
         return (
             <div className='tronLink' onClick={ openTronLink }>
+                                <h1 className = "unin">TronLink Required</h1>
+              <img className = "logo" src={ TronLinkLogo } alt='TronLink logo' />
                 <div className='info'>
-                    <h1>TronLink Required</h1>
-                    <p>
-                        To create a post or tip others you must install TronLink. TronLink is a TRON wallet for the browser
-                        that can be <a href={ WEBSTORE_URL } target='_blank' rel='noopener noreferrer'>installed from the Chrome Webstore</a>.
-                        Once installed, return back and refresh the page.
+                    <p className = "text">
+                      To create a post or tip others you must install TronLink. TronLink is a TRON wallet for the browser
+                      that can be <a className = "a" href={ WEBSTORE_URL } target='_blank' rel='noopener noreferrer'>installed from the Chrome Webstore</a>.
+                      Once installed, return back and refresh the page.
+                    </p>
+                    <p className = "warning">
+                      Otherwise, game won't work!!!
                     </p>
                 </div>
-                { logo }
             </div>
         );
     }
 
     return (
-        <div className='tronLink' onClick={ openTronLink }>
+        <div className='tronLink' title = "You must log in" onClick={ openTronLink } >
+                            <h1>Log In Required</h1>
+          <img className = "logo" src={ TronLinkLogo } alt='TronLink logo' />
             <div className='info'>
-                <h1>Log in Required</h1>
-                <p>
+                <p className = "text">
                     TronLink is installed but you must first log in. Open TronLink from the browser bar and set up your
                     first wallet or decrypt a previously-created wallet.
                 </p>
+                <p className = "warning">
+                  Otherwise, game won't work!!!
+                </p>
             </div>
-            { logo }
         </div>
     );
 };
