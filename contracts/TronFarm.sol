01a133b6e3098eeb141a00e38161fac6ac0d0951
Sofiia Teplova <sofiia.teplova@gmail.com>
2023-01-11 00:59:18 +0100

    Fix contracts

--- contracts/TronFarm.sol
+++ contracts/TronFarm.sol
@@ -1,98 +1,98 @@
-pragma solidity ^0.4.23;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
 
 contract TronFarm {
+    uint256 constant coinPrice = 12500;
+    uint256 constant animals = 5;
+    uint256 constant period = 1 hours;
 
-    uint constant coinPrice = 12500;
-    uint constant animals = 5;
-    uint constant period = 1 hours;
+    uint256[animals] prices = [8400, 30000, 100000, 230400, 458800];
+    uint256[animals] profit = [11, 40, 136, 320, 650];
+    uint256 perPrice = 2400;
+    uint256 startCoe = 100;
 
-    uint[animals] prices = [8400, 30000, 100000, 230400, 458800];
-    uint[animals] profit = [11, 40, 136, 320, 650];
-    uint perPrice = 2400;
-    uint startCoe = 100;
-
-    uint public last;
-    uint public totalPlayers;
-    uint public totalAnimals;
-    uint public totalPayout;
+    uint256 public last;
+    uint256 public totalPlayers;
+    uint256 public totalAnimals;
+    uint256 public totalPayout;
     address public owner;
 
     struct Player {
-        uint coinsReturned;
-        uint usedCoins;
-        uint allCoins;
-        uint time;
-        uint[animals] Animals;
-        uint coe;
-        uint frac;
+        uint256 coinsReturned;
+        uint256 usedCoins;
+        uint256 allCoins;
+        uint256 time;
+        uint256[animals] Animals;
+        uint256 coe;
+        uint256 frac;
     }
 
     mapping(address => Player) public players;
 
     constructor() public {
         owner = msg.sender;
-        last = now;
+        last = block.timestamp;
     }
 
-    function deposit() public payable returns(bool success){
+    function deposit() public payable returns (bool success) {
         require(msg.value >= coinPrice);
 
         Player storage player = players[msg.sender];
-        uint startCoins = player.allCoins;
+        uint256 startCoins = player.allCoins;
         player.allCoins = startCoins + (msg.value / coinPrice);
         player.usedCoins = player.usedCoins + msg.value / coinPrice;
 
         if (player.time == 0) {
-            player.time = now;
+            player.time = block.timestamp;
             totalPlayers++;
             player.coe = startCoe;
         }
         return true;
     }
 
-    function buy(uint _type, uint _number) public returns(bool success){
+    function buy(uint256 _type, uint256 _number) public returns (bool success) {
         require(_type < animals && _number > 0);
         collect(msg.sender);
 
-        uint paymentCoins = prices[_type] * _number;
+        uint256 paymentCoins = prices[_type] * _number;
         Player storage player = players[msg.sender];
 
         require(paymentCoins <= player.allCoins);
         player.allCoins = player.allCoins - paymentCoins;
 
         player.Animals[_type] = player.Animals[_type] + _number;
-        players[owner].allCoins = players[owner].allCoins +  (paymentCoins / 10);
+        players[owner].allCoins = players[owner].allCoins + (paymentCoins / 10);
 
         totalAnimals = totalAnimals + _number;
         return true;
     }
 
-    function withdraw(uint _coins) public returns (bool success){
+    function withdraw(uint256 _coins) public returns (bool success) {
         require(_coins > 0);
         collect(msg.sender);
         require(_coins <= (players[msg.sender].allCoins));
 
         transfer(msg.sender, _coins * coinPrice);
         return true;
-     }
+    }
 
     function collect(address _addr) internal {
         Player storage player = players[_addr];
         require(player.time > 0);
         require(address(this).balance >= 0);
 
-        uint hoursAdded = (now - player.time) / period;
+        uint256 hoursAdded = (block.timestamp - player.time) / period;
         if (hoursAdded > 0) {
-            uint hourlyProfit;
-            for (uint i = 0; i < animals; i++) {
+            uint256 hourlyProfit;
+            for (uint256 i = 0; i < animals; i++) {
                 hourlyProfit = hourlyProfit + (player.Animals[i] * profit[i]);
             }
-            uint earned = (hoursAdded * hourlyProfit * player.coe / 100);
-            uint q = (hoursAdded * hourlyProfit * player.coe)%100;
-            if(player.frac+q>=100){
-                player.frac -= (100-q);
+            uint256 earned = ((hoursAdded * hourlyProfit * player.coe) / 100);
+            uint256 q = (hoursAdded * hourlyProfit * player.coe) % 100;
+            if (player.frac + q >= 100) {
+                player.frac -= (100 - q);
                 earned += 1;
-            }else{
+            } else {
                 player.frac += q;
             }
             player.allCoins = player.allCoins + earned;
@@ -100,26 +100,41 @@ contract TronFarm {
         }
     }
 
-    function transfer(address _to, uint _amount) internal returns(bool success){
+    function transfer(address _to, uint256 _amount)
+        internal
+        returns (bool success)
+    {
         if (_amount > 0 && _to != address(0)) {
-            uint contractBalance = address(this).balance;
+            uint256 contractBalance = address(this).balance;
             if (contractBalance > 0) {
-                uint payout = _amount > contractBalance ? contractBalance : _amount;
+                uint256 payout = _amount > contractBalance
+                    ? contractBalance
+                    : _amount;
                 totalPayout = totalPayout + payout;
-                players[msg.sender].allCoins = players[msg.sender].allCoins - payout/coinPrice;
-                players[msg.sender].coinsReturned = players[msg.sender].coinsReturned + payout/coinPrice;
-                msg.sender.transfer(payout);
-                last = now;
+                players[msg.sender].allCoins =
+                    players[msg.sender].allCoins -
+                    payout /
+                    coinPrice;
+                players[msg.sender].coinsReturned =
+                    players[msg.sender].coinsReturned +
+                    payout /
+                    coinPrice;
+                payable(msg.sender).transfer(payout);
+                last = block.timestamp;
                 return true;
             }
         }
     }
 
-    function animalsOf(address _addr) public view returns (uint [animals]) {
+    function animalsOf(address _addr)
+        public
+        view
+        returns (uint256[animals] memory)
+    {
         return players[_addr].Animals;
     }
 
-    function setCoe(uint _per) public{
+    function setCoe(uint256 _per) public {
         Player storage player = players[msg.sender];
         collect(msg.sender);
 
@@ -127,9 +142,11 @@ contract TronFarm {
         require(_per * perPrice <= player.allCoins);
         require(_per + player.coe <= 105);
         player.allCoins = player.allCoins - (_per * perPrice);
-        players[owner].allCoins = players[owner].allCoins +  (_per * perPrice)/10;
+        players[owner].allCoins =
+            players[owner].allCoins +
+            (_per * perPrice) /
+            10;
 
         player.coe = player.coe + _per;
     }
-
 }
